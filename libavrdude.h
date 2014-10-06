22c6f995c6382d7cf58866e17fa145facd211000
zoobab <zoobab@gmail.com>
2014-10-06 16:41:21 +0200

    libavrdude.h: PIN_MAX to 255 instead of 31

--- libavrdude.h
+++ libavrdude.h
@@ -360,7 +360,7 @@ enum {
 #define PIN_MASK    (UINT_MAX>>1)
 #define PIN_INVERSE (~(PIN_MASK))	/* flag for inverted pin in serbb */
 #define PIN_MIN     0   /* smallest allowed pin number */
-#define PIN_MAX     31  /* largest allowed pin number */
+#define PIN_MAX     255  /* largest allowed pin number */
 
 #ifdef HAVE_LINUXGPIO
 /* Embedded systems might have a lot more gpio than only 0-31 */
